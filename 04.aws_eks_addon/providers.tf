83bd575166b0f8caaf591e71b5f1ccf61837d28b
junho <lwnsgh@mz.co.kr>
2025-05-28 18:35:46 +0900

    helm provider 추가

--- 04.aws_eks_addon/providers.tf
+++ 04.aws_eks_addon/providers.tf
@@ -5,3 +5,15 @@ provider "aws" {
     tags = local.common_tags
   }
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks_workspace.outputs.eks_cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.this.token
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks_workspace.outputs.eks_cluster_ca_data)
+  }
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = data.terraform_remote_state.eks_workspace.outputs.eks_cluster_name
+}

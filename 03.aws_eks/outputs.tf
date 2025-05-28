47ed46450271c7e7d11ddf1c91770a2f2dfc63fc
junho <lwnsgh@mz.co.kr>
2025-05-28 18:31:49 +0900

    output(eks endpoint, ca) 추가

--- 03.aws_eks/outputs.tf
+++ 03.aws_eks/outputs.tf
@@ -17,3 +17,14 @@ output "eks_oidc_provider_arn" {
   description = "EKS OIDC Provider ARN"
   value       = module.eks.oidc_provider_arn
 }
+
+output "eks_cluster_endpoint" {
+  description = "EKS API 서버 엔드포인트 (Helm provider host)"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_ca_data" {
+  description = "EKS 클러스터 CA 인증서(base64, Helm provider cluster_ca_certificate)"
+  value       = module.eks.cluster_certificate_authority_data
+}
+

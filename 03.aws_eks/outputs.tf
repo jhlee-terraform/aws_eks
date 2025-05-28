54f9e91b32251b308c16b417acf11ee0b5e47856
junho <lwnsgh@mz.co.kr>
2025-05-28 17:09:03 +0900

    output(eks_oidc_provider_arn) 추가

--- 03.aws_eks/outputs.tf
+++ 03.aws_eks/outputs.tf
@@ -12,3 +12,8 @@ output "route53_zone_id" {
   description = "Route53 호스팅 존 ID"
   value       = aws_route53_zone.hosting_zone.zone_id
 }
+
+output "eks_oidc_provider_arn" {
+  description = "EKS OIDC Provider ARN"
+  value       = module.eks.oidc_provider_arn
+}
